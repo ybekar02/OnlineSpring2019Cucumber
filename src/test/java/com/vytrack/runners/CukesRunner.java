c2248c316450890083175765406831a83157c845
CybertekSchool <github17>
2019-08-04 17:07:36 -0400

    added feature file

---
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.vytrack.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {
+            "src/test/resources/features" //to specify where are the features
+        },
+        //feature contains scenarios
+        //every scenario is like a test
+        //where is the implementation for features
+        glue = {"com/vytrack/step_definitions/LoginStepDefinitions.java"},
+        //dry tun - to generate step definitions automatically
+        //you will see them in the console output
+        dryRun = true
+
+)
+public class CukesRunner {
+}
